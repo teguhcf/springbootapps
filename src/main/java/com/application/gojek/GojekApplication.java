2456c40928278e872204c5cdbdb29b175045b4f3
teguhcf <teguhchandraf@gmail.com>
2019-03-15 19:19:12 +0700

    First commit

---
+++ src/main/java/com/application/gojek/GojekApplication.java
@@ -0,0 +1,13 @@
+package com.application.gojek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GojekApplication.class, args);
+	}
+
+}
